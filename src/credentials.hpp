3b96bb572c1ce06014ceb31681d6029e23907d2d
Jennifer Michelle Rath <jemira@quelloffen.net>
2023-05-07 17:59:52 +0200

    Wifi funktioniert

---
+++ src/credentials.hpp
@@ -0,0 +1,8 @@
+#ifndef _CREDENTIALS_HPP
+#define _CREDENTIALS_HPP
+
+extern char wifi_ssid[];
+extern char wifi_pass[];
+extern char wifi_host[];
+
+#endif // _CREDENTIALS_HPP //
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#include "credentials.hpp"
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.hostname(wifi_host); 
+  WiFi.begin(wifi_ssid, wifi_pass);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.write("Oeha\n");
   digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
   delay(500);                      // wait for a second
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage LOW
