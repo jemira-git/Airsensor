56f628f5cfad1e3d9dd7e4faa85757b34bd83c9d
Jennifer Michelle Rath <jemira@quelloffen.net>
2023-05-07 22:31:51 +0200

    co2Sensor arbeitet

--- src/credentials.hpp
+++ src/credentials.hpp
@@ -1,8 +1,8 @@
 #ifndef _CREDENTIALS_HPP
 #define _CREDENTIALS_HPP
 
-extern char wifi_ssid[];
-extern char wifi_pass[];
-extern char wifi_host[];
+extern const char wifi_ssid[];
+extern const char wifi_pass[];
+extern const char wifi_host[];
 
 #endif // _CREDENTIALS_HPP //
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>                                   
+#include <SoftwareSerial.h> 
 
 #include "credentials.hpp"
+#include "MHZ19.h"   
+
+MHZ19 myMHZ19;                                             // Constructor for library
+SoftwareSerial serial2(D1, D2);                            // (Uno example) create device to MH-Z19 serial
+unsigned long getDataTimer = 0;
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin LED_BUILTIN as an output:
   pinMode(LED_BUILTIN, OUTPUT);
+
+  // Initialize serial monitor (UART0):
   Serial.begin(115200);
   Serial.println();
 
+  // Initialize soft serial for CO2 sensor:
+  serial2.begin(9600);   
+  myMHZ19.begin(serial2);
+
+  // Turn auto calibration ON (OFF autoCalibration(false)):
+  myMHZ19.autoCalibration();                             
+
+  // Connect to WiFi:
   WiFi.hostname(wifi_host); 
   WiFi.begin(wifi_ssid, wifi_pass);
 
@@ -26,9 +42,26 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.write("Oeha\n");
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED off by making the voltage LOW
-  delay(3000);                      // wait for a second
+  // Do not block in main loop to allow back processes
+  // to run:
+  if (millis() - getDataTimer >= 2000) {
+
+    // Request CO2 as ppm:
+    int co2 =  myMHZ19.getCO2();
+    Serial.print("CO2 (ppm): ");                      
+    Serial.println(co2);
+
+    // Request Temperature (as Celsius):
+    int8_t temp = myMHZ19.getTemperature(); 
+    Serial.print("Temperature (C): ");                  
+    Serial.println(temp);
+
+    // Flash the LED for visual feedback:
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(50);                     
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // Update milliseconds variable for loop:
+    getDataTimer = millis();
+  }
 }
